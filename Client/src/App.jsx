b9bd2276aaba4b4e32d35f9756061f2db5075eca
anish0905 <anishanish0102@gmail.com>
2024-07-30 17:16:37 +0530

    added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="bg-red-900">hELLO</h1>
+      <div className="bg-red-800">
+        <HomePage />
+      </div>
     </>
   );
 }
---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <ul className="bg-red-400  ">
+          <li className="text-red-800">HomeLine Logo</li>
+        </ul>
+        <ul>
+          <li>Search bar</li>
+          <li>Login</li>
+          <li>Signup</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
