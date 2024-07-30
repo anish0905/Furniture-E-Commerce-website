f05e89340d97a34fbdcbb0a1ed49a51636bf48b1
Ramnarayan Mandal <154451608+RamnarayanMandal@users.noreply.github.com>
2024-07-30 17:18:20 +0530

    add first file

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <>
-      <h1 className="bg-red-900">hELLO</h1>
+      
+      <HomePage/>
     </>
   );
 }
---
+++ Client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-black text-center text-white py-4'>
+      <h1>hi.........................</h1>
+    </div>
+  )
+}
+
+export default HomePage
